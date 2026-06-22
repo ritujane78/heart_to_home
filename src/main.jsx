fd9a5372c9bdb6cd2b9d73946791ac598a0cb230
Ritu Bafna <ritujane78@gmail.com>
2026-06-22 17:15:35 +0545

    split css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './styles.css';
+import './styles/main.css';
+import './styles/components.css';
+import './styles/pages/home.css';
+import './styles/pages/services.css';
+import './styles/pages/gift-form.css';
+import './styles/pages/contact.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
